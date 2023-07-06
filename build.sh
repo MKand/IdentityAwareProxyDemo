6a92f84c10336024541393f375b3fc6e38e0f0fc
Manasa Kandula <manasakandula.m@gmail.com>
2023-07-06 08:21:59 +0000

    initial commit

---
+++ build.sh
@@ -0,0 +1,16 @@
+# Enable Cloudbuild API
+echo -e "\e[95mEnabling required APIs in ${PROJECT_ID}\e[0m"
+gcloud services enable cloudbuild.googleapis.com storage.googleapis.com artifactregistry.googleapis.com beyondcorp.googleapis.com cloudapis.googleapis.com compute.googleapis.com iap.googleapis.com
+
+# Make cloudbiuld SA roles/owner for PROJECT_ID
+# TODO: Make these permissions more granular to precisely what is required by cloudbuild
+echo -e "\e[95mAssigning Cloudbuild Service Account roles/owner in ${PROJECT_ID}\e[0m"
+export PROJECT_NUMBER=$(gcloud projects describe ${PROJECT_ID} --format 'value(projectNumber)')
+gcloud projects add-iam-policy-binding ${PROJECT_ID} --member serviceAccount:${PROJECT_NUMBER}@cloudbuild.gserviceaccount.com --role roles/owner
+
+# Start main build
+echo -e "\e[95mStarting Cloudbuild to create infrastructure using ${BUILD}...\e[0m"
+[[ "${DESTROY}" != "true" ]] && gcloud builds submit --config=builds/infra_terraform.yaml --substitutions=_PROJECT_ID=${PROJECT_ID} --async
+#[[ "${DESTROY}" == "true" ]] && gcloud builds submit --config=builds/infra_terraform_destroy.yaml --substitutions=_PROJECT_ID=${PROJECT_ID} --async
+
+echo -e "\e[95mYou can view the Cloudbuild status through https://console.cloud.google.com/cloud-build\e[0m"
---
+++ pythonapp/src/app.py
@@ -0,0 +1,62 @@
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import platform
+
+import flask
+
+import validate_jwt
+
+import os
+
+CLOUD_PROJECT_ID = os.getenv("PROJECT_ID", "abc")
+BACKEND_SERVICE_ID = os.getenv("BACKEND_SERVICE_ID", "abc")
+SERVICE_HOST = os.getenv("SERVICE_HOST", "UNKNOWN")
+PROXY_HOST = os.getenv("PROXY_HOST", "localhost:5000")
+app = flask.Flask(__name__)
+
+
+
+
+@app.route("/")
+def root():
+    jwt = flask.request.headers.get("x-goog-iap-jwt-assertion")
+
+    app.logger.info("JWT:", jwt)
+
+    if jwt is None:
+        return "Unauthorized request.", 401
+    expected_audience = (
+        f"/projects/{CLOUD_PROJECT_ID}/global/backendServices/{BACKEND_SERVICE_ID}"
+    )
+
+    user_id, user_email, error_str = validate_jwt.validate_iap_jwt(
+        jwt, expected_audience
+    )
+    if error_str:
+        return f"Error: {error_str}"
+    else:
+        return f"Hi, {user_email}. I am {platform.node()}."
+
+@app.route("/proxyhello") 
+#TODO implement proxying
+def helloproxy():
+    return "hello back from " + SERVICE_HOST + " " + platform.node() + "\n"
+
+@app.route("/hello") 
+def hello():
+    return "hello back from " + SERVICE_HOST  + " " + platform.node() + "\n"
+
+if __name__ == "__main__":
+    app.run(debug=True)
